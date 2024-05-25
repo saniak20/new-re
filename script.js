561f87bfeedc4a583c6ae569ecf4d9174c8825e0
saniak20 <163977809+saniak20@users.noreply.github.com>
2024-05-25 07:25:58 +0530

    Add files via upload
    This is the camera by html and css

---
+++ script.js
@@ -0,0 +1,6 @@
+const camera =document.getElementById("camera");
+const canvasElement=document.getElementById("canvas");
+async function showCamera(){
+    await navigator MediaDevices getUserMedia
+
+}
